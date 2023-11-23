7953b5154ce44640c152f93cc42414138b678891
_userNull <ramzprivatexd@gmail.com>
2023-11-23 21:45:07 +0700

    initial commit

--- indo_java/src/dashboard/Dashboard.java
+++ indo_java/src/dashboard/Dashboard.java
@@ -1,12 +1,30 @@
 package dashboard;
 
-import java.io.IOException;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 
 public class Dashboard {
-    
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button btnDashboard;
+
+    @FXML
+    void hover(MouseEvent event) {
+        btnDashboard.setStyle("-fx-background-color: rgb(116, 54, 250, 0.3)");
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+
 }
